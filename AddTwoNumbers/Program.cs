5120deae6572797543bf58911af923cd74c17c32
Jeff Lee <hellow.jeff@gmail.com>
2018-12-10 10:28:11 +0800

    add new problem - AddTwoNumbers

---
+++ AddTwoNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+        //Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
+
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return null;
+        }
+    }
+}
