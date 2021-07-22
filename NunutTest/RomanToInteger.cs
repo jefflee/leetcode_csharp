fa533b2c3333101101b1202fa58653d8b3312c95
Jeff Lee <jeff.lee@redcypress.com.tw>
2021-07-22 08:08:52 +0800

    Roman to integer

---
+++ NunutTest/RomanToInteger.cs
@@ -0,0 +1,76 @@
+using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NunutTest
+{
+    public class RomanToInteger
+    {
+        static readonly IDictionary<char, int> romanValueDic = new Dictionary<char, int>()
+        {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+
+        };
+        static readonly IDictionary<string, int> comboValueDic = new Dictionary<string, int>()
+        {
+            {"IV", 4},
+            {"IX", 9},
+            {"XL", 40},
+            {"XC", 90},
+            {"CD", 400},
+            {"CM", 900},
+        };
+        public int RomanToInt(string s)
+        {
+            int result = 0;
+            for (int k = 0; k < s.Length; k++)
+            {
+                if (k < s.Length - 1)
+                {
+                    var comboValue = GetComboValue(s, k);
+                    if (comboValue > 0)
+                    {
+                        result += comboValue;
+                        k++;
+                        continue;
+                    }
+                }
+
+                if (romanValueDic.ContainsKey(s[k]))
+                {
+                    result += romanValueDic[s[k]];
+                }
+            }
+            return result;
+        }
+
+        private static int GetComboValue(string s, int k)
+        {
+            string combo = s[k] + s[k + 1].ToString();
+            return comboValueDic.ContainsKey(combo) ? comboValueDic[combo] : 0;
+        }
+    }
+
+    public class RomanToIntegerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            RomanToInteger convert = new RomanToInteger();
+            int val = convert.RomanToInt("III");
+
+            val.Should().Be(3);
+        }
+    }
+}
