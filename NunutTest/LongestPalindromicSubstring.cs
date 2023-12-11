87d4ff7645c4a18b141016d86b12b43a8a133a5e
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2023-12-12 07:00:36 +0800

    temp

--- NunutTest/LongestPalindromicSubstring.cs
+++ NunutTest/LongestPalindromicSubstring.cs
@@ -5,17 +5,13 @@ namespace NunitTest;
 
 /// <summary>
 ///     https://leetcode.com/problems/longest-palindromic-substring/description/
+///     Solution1: Brute force approach. Compare direction is from two sides to middle.
 /// </summary>
-internal class LongestPalindromicSubstring
+internal class LongestPalindromicSubstringSolution1
 {
     public string LongestPalindrome(string s)
     {
-        if (string.IsNullOrEmpty(s))
-        {
-            return string.Empty;
-        }
-
-        if (s.Length == 1)
+        if (s.Length <= 1)
         {
             return s;
         }
@@ -36,13 +32,11 @@ internal class LongestPalindromicSubstring
         return maxPalindrome;
     }
 
-    private bool IsPalindrome(string input)
+    private static bool IsPalindrome(string input)
     {
         for (var k = 0; k < input.Length / 2; k++)
         {
-            var leftChar = input[k];
-            var rightChar = input[^(k + 1)];
-            if (leftChar != rightChar)
+            if (input[k] != input[^(k + 1)])
             {
                 return false;
             }
@@ -52,15 +46,55 @@ internal class LongestPalindromicSubstring
     }
 }
 
+/// <summary>
+///     https://leetcode.com/problems/longest-palindromic-substring/solutions/4212564/beats-96-49-5-different-approaches-brute-force-eac-dp-ma-recursion/
+///     https://medium.com/hoskiss-stand/manacher-299cf75db97e
+/// </summary>
+internal class ManacherAlgorithm
+{
+    public string LongestPalindrome(string s)
+    {
+        if (s.Length <= 1)
+        {
+            return s;
+        }
+
+        // Insert # between each char
+        var reformatedString = $"#{string.Join('#', s.ToCharArray())}#";
+        var maxPalindrome = reformatedString[..1];
+        var dp = new int[reformatedString.Length];
+
+        for (var k = 0; k < reformatedString.Length; k++)
+        {
+            var left = 1;
+            var right = 1;
+            while (reformatedString[k - left] == reformatedString[k + right])
+            {
+                dp[k]++;
+            }
+        }
+
+        return reformatedString;
+    }
+}
+
 public class LongestPalindromicSubstringTest
 {
     [TestCase("babad", "bab")]
     [TestCase("cbbd", "bb")]
     [TestCase("abc1cba", "abc1cba")]
     [TestCase("1qaabc1cbazxc", "abc1cba")]
-    public void Test(string input, string expected)
+    public void LongestPalindromicSubstringSolution1Test(string input, string expected)
     {
-        var sut = new LongestPalindromicSubstring();
+        var sut = new LongestPalindromicSubstringSolution1();
+        var output = sut.LongestPalindrome(input);
+        output.Should().Be(expected);
+    }
+
+    [TestCase("babad", "bab")]
+    public void ManacherAlgorithmTest(string input, string expected)
+    {
+        var sut = new ManacherAlgorithm();
         var output = sut.LongestPalindrome(input);
         output.Should().Be(expected);
     }
