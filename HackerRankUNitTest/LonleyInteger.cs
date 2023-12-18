22ba101a71e5fef645c541a5bdd9ebbabb387daa
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2023-12-19 06:45:35 +0800

    Add lonleyInteger and MaximumSubarray

---
+++ HackerRankUNitTest/LonleyInteger.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+
+namespace HackerRankUNitTest;
+
+/// <summary>
+///     https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem
+/// </summary>
+internal class BruteForceSolution
+{
+    public int Lonelyinteger(List<int> a)
+    {
+        var countDic = new Dictionary<int, int>();
+        foreach (var num in a)
+        {
+            if (countDic.ContainsKey(num))
+            {
+                countDic[num] += 1;
+            }
+            else
+            {
+                countDic[num] = 1;
+            }
+        }
+
+        foreach (var entry in countDic)
+        {
+            if (entry.Value == 1)
+            {
+                return entry.Key;
+            }
+        }
+
+        return 0;
+    }
+}
+
+internal class XorSolution
+{
+    public int Lonelyinteger(List<int> a)
+    {
+        var ans = 0;
+        foreach (var number in a)
+        {
+            ans = ans ^ number;
+        }
+
+        return ans;
+    }
+}
+
+internal class LonleyIntegerTest
+{
+    [TestCaseSource(typeof(TestCases))]
+    public void BruteForceSolutionTest(List<int> numbers, int expectedNumber)
+    {
+        var sut = new BruteForceSolution();
+        var output = sut.Lonelyinteger(numbers);
+        output.Should().Be(expectedNumber);
+    }
+
+    [TestCaseSource(typeof(TestCases))]
+    public void XorSolutionTest(List<int> numbers, int expectedNumber)
+    {
+        var sut = new XorSolution();
+        var output = sut.Lonelyinteger(numbers);
+        output.Should().Be(expectedNumber);
+    }
+}
+
+public class TestCases : IEnumerable
+{
+    public IEnumerator GetEnumerator()
+    {
+        yield return new object?[] { new List<int> { 1, 2, 3, 4, 3, 2, 1 }, 4 };
+    }
+}
---
+++ NunutTest/DpProblems/MaximumSubarray.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+
+namespace LeetCodeNUnitTest.DpProblems;
+
+/// <summary>
+///     https://leetcode.com/problems/maximum-subarray/description/
+///     50. Maximum Subarray
+///     Dynamic Programming
+/// </summary>
+internal class MaximumSubarrayDpsolution
+{
+    public int MaxSubArray(int[] nums)
+    {
+        if (nums.Length == 1)
+        {
+            return nums[0];
+        }
+
+        var maxSum = nums[0];
+        var ans = nums[0];
+
+        for (var k = 1; k < nums.Length; k++)
+        {
+            var summedMax = nums[k] + maxSum;
+            maxSum = Math.Max(nums[k], summedMax);
+
+            ans = Math.Max(ans, maxSum);
+        }
+
+        return ans;
+    }
+}
+
+public class MaximumSubarrayTest
+{
+    [TestCaseSource(typeof(TestCases))]
+    public void MaximumSubarrayDpsolutionTest(int[] nums, int expected)
+    {
+        var sut = new MaximumSubarrayDpsolution();
+        var output = sut.MaxSubArray(nums);
+        output.Should().Be(expected);
+    }
+
+    public class TestCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object?[] { new[] { 2, 3, -2, 4 }, 7 };
+            yield return new object?[] { new[] { -2, 0, -1 }, 0 };
+            yield return new object?[] { new[] { -2, 0, -1, -3 }, 0 };
+            yield return new object?[] { new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }, 6 };
+            yield return new object?[] { new[] { 5, 4, -1, 7, 8 }, 23 };
+            yield return new object?[] { new[] { 1 }, 1 };
+        }
+    }
+}
