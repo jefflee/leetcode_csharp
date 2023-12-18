7088c3757967d0e8d55cd3263d6f1cb89474f3c8
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2023-12-19 06:45:42 +0800

    temp

---
+++ NunutTest/DpProblems/DegreeOfAnArray.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace LeetCodeNUnitTest.DpProblems;
+
+/// <summary>
+///     https://leetcode.com/problems/degree-of-an-array/description/
+///     697. Degree of an Array
+/// </summary>
+internal class DegreeOfAnArraySolution
+{
+    public int FindShortestSubArray(int[] nums)
+    {
+        var mostFrequentInt = nums.GroupBy(p => p).MaxBy(p => p.Count());
+        var arrayDegree = mostFrequentInt!.Count();
+
+
+        return 0;
+    }
+}
+
+internal class DegreeOfAnArrayyTest
+{
+    [TestCaseSource(typeof(TestCases))]
+    public void DegreeOfAnArrayTest(int[] nums, int expected)
+    {
+        var sut = new DegreeOfAnArraySolution();
+        var output = sut.FindShortestSubArray(nums);
+        output.Should().Be(expected);
+    }
+
+    public class TestCases : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            yield return new object?[] { new[] { 1, 2, 2, 3, 1 }, 2 };
+            yield return new object?[] { new[] { 1, 2, 2, 3, 1, 4, 2 }, 6 };
+        }
+    }
+}
